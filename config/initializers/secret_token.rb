2534019713e65d5c27c79e444290e5524e7bb55d
dyw <dyw8086@163.com>
2011-10-29 20:49:04 +0800

    First commit asktop project using rails3

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Asktop::Application.config.secret_token = '3b51a9366ecaa2cab449eeef4bd67e37d83f03d2cf5da0961030e45444eba3a91a193e81399ec1c191a4316e1fdf4e5bc42d697d82cbced7925f65bb1dd640da'
